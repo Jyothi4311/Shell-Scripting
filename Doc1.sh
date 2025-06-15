3336af1c0f3ba10267572f0b33de5fd462e86a7d
bh23301 <jyothi.veeranala@macys.com>
2025-06-15 16:03:55 +0530

    document

---
+++ Doc1.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+MSG="Document-11"
+GREET="Hi from Document-1"
+source ./Doc2.sh
+
+echo "Hello from: $MSG"
+echo "A value: $A"
+
+#sh 21-script-2.sh
---
+++ Doc2.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+MSG="Document-2"
+A="10"
+
+echo "Hello from: $MSG"
+
+echo "Greeting: $GREET"
