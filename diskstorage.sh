4fe1b0185b26179c80c5219243d1009e17326a7d
bh23301 <jyothi.veeranala@macys.com>
2025-06-15 15:59:10 +0530

    storage

--- diskstorage.sh
+++ diskstorage.sh
@@ -17,4 +17,4 @@ done <<< $DISK_USAGE
 
 echo -e "Message: $MSG"
 
-echo "$MSG" | mutt -s "High Disk Usage" jagadeeshposa@gmail.com
+echo "$MSG" | mutt -s "High Disk Usage" jyothiveeranala@gmail.com
