90890c08d98d20f628bac9cccba68cddaeb38572
bh23301 <jyothi.veeranala@macys.com>
2025-06-15 16:21:50 +0530

    sourcetext

--- commontext.sh
+++ commontext.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
-source ./commontext.sh
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
 
-SOURCE_DIR="/home/ec2-user/app-logs"
-echo "Script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
+LOGS_FOLDER="/var/log/shellscript-logs"
+LOG_FILE=$(echo $0 | cut -d "." -f1 )
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
 
-FILES_TO_DELETE=$(find $SOURCE_DIR -name "*.log" -mtime +14)
-echo "Files to be deleted: $FILES_TO_DELETE"
-
-while read -r filepath # here filepath is the variable name, you can give any name
-do
-    echo "Deleting file: $filepath" &>>$LOG_FILE_NAME
-    rm -rf $filepath
-    echo "Deleted file: $filepath"
-done <<< $FILES_TO_DELETE
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 ... $R FAILURE $N"
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N"
+    fi
+}
